7ef273a733f600b9e8c825be03c857d23c92e6ce
Asaad Almutareb <asaad@mutareb.com>
2024-03-05 21:40:50 +0000

    moved ollama agent to it's own folder added mixtral agent switched from serpapi to googleSearchAPI added sources urls to final output

--- innovation_pathfinder_ai/structured_tools/structured_tools.py
+++ innovation_pathfinder_ai/structured_tools/structured_tools.py
@@ -1,6 +1,10 @@
 from langchain.tools import BaseTool, StructuredTool, tool
-from langchain.retrievers import ArxivRetriever
-from langchain_community.utilities import SerpAPIWrapper
+from langchain_community.retrievers import ArxivRetriever
+#from langchain_community.utilities import SerpAPIWrapper
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+#from langchain.tools import Tool
+from langchain_community.utilities import GoogleSearchAPIWrapper
 import arxiv
 
 # hacky and should be replaced with a database
@@ -10,7 +14,8 @@ from innovation_pathfinder_ai.source_container.container import (
 
 @tool
 def arxiv_search(query: str) -> str:
-    """Using the arxiv search and collects metadata."""
+    """Search arxiv database for scientific research papers and studies. This is your primary information source.
+    always check it first when you search for information, before using any other tool."""
     # return "LangChain"
     global all_sources
     arxiv_retriever = ArxivRetriever(load_max_docs=2)
@@ -40,23 +45,34 @@ def get_arxiv_paper(paper_id:str) -> None:
     number_without_period = paper_id.replace('.', '')
     
     # Download the PDF to a specified directory with a custom filename.
-    paper.download_pdf(dirpath="./mydir", filename=f"{number_without_period}.pdf")
+    paper.download_pdf(dirpath="./downloaded_papers", filename=f"{number_without_period}.pdf")
     
     
 @tool
 def google_search(query: str) -> str:
-    """Using the google search and collects metadata."""
+    """Search Google for additional results when you can't answer questions using arxiv search or wikipedia search."""
     # return "LangChain"
     global all_sources
     
-    x = SerpAPIWrapper()
-    search_results:dict = x.results(query)
+    websearch = GoogleSearchAPIWrapper()
+    search_results:dict = websearch.results(query, 5)
     
  
-    organic_source = search_results['organic_results']
+    #organic_source = search_results['organic_results']
     # formatted_string = "Title: {title}, link: {link}, snippet: {snippet}".format(**organic_source)
-    cleaner_sources = ["Title: {title}, link: {link}, snippet: {snippet}".format(**i) for i in organic_source]
+    cleaner_sources = ["Title: {title}, link: {link}, snippet: {snippet}".format(**i) for i in search_results]
     
     all_sources += cleaner_sources    
     
     return cleaner_sources.__str__()
+
+@tool
+def wikipedia_search(query: str) -> str:
+    """Search Wikipedia for additional information to expand on research papers or when no papers can be found."""
+    global all_sources
+
+    api_wrapper = WikipediaAPIWrapper()
+    wikipedia_search = WikipediaQueryRun(api_wrapper=api_wrapper)
+    wikipedia_results = wikipedia_search.run(query)
+    all_sources += wikipedia_results
+    return wikipedia_results
