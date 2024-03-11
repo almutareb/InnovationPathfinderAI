a4dffa4f0c3071a6a9eb9d1b84d549d6acb39916
Isayah Culbertson <isayahCulbertson@gmail.com>
2024-03-11 20:53:31 +0000

    now allow for data to be added to already existing collections

--- innovation_pathfinder_ai/vector_store/chroma_vector_store.py
+++ innovation_pathfinder_ai/vector_store/chroma_vector_store.py
@@ -1,4 +1,8 @@
-# got some of the code from https://diptimanrc.medium.com/rapid-q-a-on-multiple-pdfs-using-langchain-and-chromadb-as-local-disk-vector-store-60678328c0df
+# got some of the code from 
+# https://diptimanrc.medium.com/rapid-q-a-on-multiple-pdfs-using-langchain-and-chromadb-as-local-disk-vector-store-60678328c0df
+# https://stackoverflow.com/questions/76482987/chroma-database-embeddings-none-when-using-get
+# https://docs.trychroma.com/embeddings/hugging-face?lang=py
+# https://www.datacamp.com/tutorial/chromadb-tutorial-step-by-step-guide
 
 import PyPDF2
 import io
@@ -9,12 +13,25 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 import chromadb
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import chromadb.utils.embedding_functions as embedding_functions
 import dotenv
+from tqdm import tqdm
+import uuid
+
 
 dotenv.load_dotenv()
 
 
 VECTOR_DATABASE_LOCATION = os.getenv("VECTOR_DATABASE_LOCATION")
+
+def generate_uuid() -> str:
+    """
+    Generate a UUID (Universally Unique Identifier) and return it as a string.
+
+    Returns:
+        str: A UUID string.
+    """
+    return str(uuid.uuid4())
 
         
 def extract_text_from_pdf(file) -> list[str]:
@@ -33,7 +50,8 @@ def extract_text_from_pdf(file) -> list[str]:
 
 
 def add_pdf_to_vector_store(
-    vector_store:Chroma.from_documents,
+    # vector_store:Chroma.from_documents,
+    collection_name,
     pdf_file_location:str,
     text_chunk_size=1000,
     text_chunk_overlap=10,
@@ -44,7 +62,7 @@ def add_pdf_to_vector_store(
     and store it the given vectorstore
     
     ## Arguments
-    vectorstore (vectorstore): 
+    collection_name (str) : name of collection to store documents
     pdf_file_location (str) : location of pdf file
     
     ## Return 
@@ -54,23 +72,45 @@ def add_pdf_to_vector_store(
     documents = []
     
     loader = PyPDFLoader(pdf_file_location)
-    # documents.extend(loader.load())
     
     text_splitter = CharacterTextSplitter(
         chunk_size=text_chunk_size,
         chunk_overlap=text_chunk_overlap,
         )
     
-    
     documents.extend(loader.load())
+    
+    client = chromadb.PersistentClient(
+    # path=persist_directory,
+    )
+    
+    # client.delete_collection(
+    # name=collection_name,
+    # )
+    
+    collection = client.get_or_create_collection(
+    name=collection_name,
+    )
+    
+    embed_data = embedding_functions.HuggingFaceEmbeddingFunction(
+        api_key= os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    )
+    
     
     chunked_documents = text_splitter.split_documents(documents)
     
-    vectordb = Chroma.from_documents(
-        documents=chunked_documents,
-        embedding = HuggingFaceEmbeddings(),
-        persist_directory=VECTOR_DATABASE_LOCATION,
-    )
+    documents_page_content:list = [i.page_content for i in documents]
+    
+    
+    for i in range(0, len(documents)):
+        data = documents[i]
+        print(i)
+        collection.add(
+            ids=[generate_uuid()], # give each document a uuid
+            documents=documents_page_content[i], # contents of document
+            embeddings=embed_data.embed_with_retries(documents_page_content[i]),
+            metadatas=data.metadata,  # type: ignore
+        )
     
     
 def load_chunk_persist_pdf(
@@ -128,9 +168,10 @@ if __name__ == "__main__":
     
     vector_db = load_vector_store()
     # pdf_file_location = "mydir/181000551.pdf"
-    pdf_file_location = "/workspaces/InnovationPathfinderAI/2402.17764.pdf"
+    # pdf_file_location = "/workspaces/InnovationPathfinderAI/2402.17764.pdf"
+    pdf_file_location = "/workspaces/InnovationPathfinderAI/2212.02623.pdf"
     
     add_pdf_to_vector_store(
-        vector_store=vector_db,
+        collection_name="ArxivPapers",
         pdf_file_location=pdf_file_location,
     )
