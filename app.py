5604c54e5139718cf3a2cbcb8293520fb3519bb9
Asaad Almutareb <asaad@mutareb.com>
2024-03-20 16:46:15 +0000

    moved the db.add for agent memory storing to be able to include the sources

--- app.py
+++ app.py
@@ -40,8 +40,6 @@ def initialize_chroma_db() -> Chroma:
 
 if __name__ == "__main__":
     
-    current_id = generate_uuid()
-    
     db = initialize_chroma_db()
     
     def add_text(history, text):
@@ -52,6 +50,17 @@ if __name__ == "__main__":
         response = infer(history[-1][0], history)
         sources = extract_urls(all_sources)
         src_list = '\n'.join(sources)
+        current_id = generate_uuid()
+        db.add(
+            ids=[current_id],
+            documents=[response['output']],
+            metadatas=[
+                {
+                    "query":history[-1][0],
+                    "sources":src_list
+                }
+            ]
+        )
         response_w_sources = response['output']+"\n\n\n Sources: \n\n\n"+src_list
         history[-1][1] = response_w_sources
         return history
@@ -63,19 +72,7 @@ if __name__ == "__main__":
                 "input": question,
                 "chat_history": history
             }
-        )
-        
-        db.add(
-            ids=[current_id],
-            documents=[result['output']],
-            metadatas=[
-                {
-                    "query":query,
-                    "sources":result['sources'].__str__()
-                }
-            ]
-        )
-        
+        )        
         return result
 
     def vote(data: gr.LikeData):
