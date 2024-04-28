a17f77f0eee24a7556f8fd7c02ff9d4bbe0247d1
Isayah Culbertson <isayahCulbertson@gmail.com>
2024-04-28 01:13:35 +0000

    gave test_data a better name

--- innovation_pathfinder_ai/test/test_vectorstore.py
+++ innovation_pathfinder_ai/test/test_vectorstore.py
@@ -18,7 +18,7 @@ from vector_store.chroma_vector_store import (
 
 class TestVectorStore(unittest.TestCase):
     def setUp(self):
-        collection_name="TestImages"
+        self.collection_name="TestImages"
         self.tmp_dir = tempfile.TemporaryDirectory()
         
         client = chromadb.PersistentClient(
@@ -26,7 +26,7 @@ class TestVectorStore(unittest.TestCase):
             )
         
         collection = client.get_or_create_collection(
-        name=collection_name,   
+        name=self.collection_name,   
         )
         
         embedding_function = SentenceTransformerEmbeddings(
@@ -35,21 +35,21 @@ class TestVectorStore(unittest.TestCase):
         
         self.vector_db = Chroma(
         client=client, # client for Chroma
-        collection_name=collection_name,
+        collection_name=self.collection_name,
         embedding_function=embedding_function,
         )
         
     def tearDown(self):
         self.tmp_dir.cleanup()
         
-    def test_data(self):
+    def test_add_image_to_vector_store(self):
         image_url = "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
         response = requests.get(image_url)
         with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
             temp_file.write(response.content)  # Write the image content to the temporary file
             temp_file_name = temp_file.name  # Get the temporary file name
             add_image_to_vector_store(
-                "TestCollectionName",
+                self.collection_name,
                 temp_file_name,
                 self.vector_db,
                 )
